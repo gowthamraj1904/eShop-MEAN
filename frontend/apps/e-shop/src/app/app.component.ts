6005992ccc5f15215dad37bed68a5f078dfc44d0
GOWTHAMRAJ S <gowtham04raj@gmail.com>
2023-03-22 11:08:47 +0530

    Created Home, Product list, Header and Footer components. Created Routes for home and products.

--- frontend/apps/e-shop/src/app/app.component.ts
+++ frontend/apps/e-shop/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'frontend-root',
+    selector: 'e-shop-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
